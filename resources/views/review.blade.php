ff54a652f5d9a0e84f3c6647965397c54661a9be
0dens1337 <did.9696@mail.ru>
2024-05-19 19:37:59 +0300

    seccond_commit

--- resources/views/review.blade.php
+++ resources/views/review.blade.php
@@ -21,4 +21,14 @@
         <textarea name="message" id="message" class="form-control" placeholder="Введите сообщение"></textarea><br>
         <button type="submit" class="btn btn-success">Отправить</button>
     </form>
+
+    <br>
+    <h1>Все отзывы</h1>
+    @foreach($reviews as $el)
+        <div class="alert alert-warning">
+            <h3>{{ $el->subject }}</h3>
+            <b>{{ $el->email }}</b>
+            <p>{{ $el->message }}</p>
+        </div>
+    @endforeach
 @endsection
